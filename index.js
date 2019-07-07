dd29a3044173e508288611e6ab0a2f2d34d41611
Jade Huynh <35jadeh@gmail.com>
2019-07-07 18:17:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+function getFirstSelector(selector)
+{
+  return document.querySelector(selector)
+}
 
+function nestedTarget()
+{
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n)
+{
+  var ranks = document.querySelectorAll('ul.ranked-list li')
+  for( var i = 0; i<ranks.length; i++)
+  {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+  }
+  
+}
+
+function deepestChild()
+{
+  var currentNode = document.querySelector('#grand-node')
+  var nextNode = currentNode.children[0]                    
+  
+  while(nextNode)                       
+  {
+    currentNode = nextNode
+    nextNode = currentNode.children[0]
+  }
+  
+  return currentNode
+}
